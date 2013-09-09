a8211d958ee84d156de4f20807f5b051c3b201b7
owain lewis <owain@owainlewis.com>
2013-09-09 19:12:37 +0100

    adds models

--- src/relevance/core.clj
+++ src/relevance/core.clj
@@ -1,5 +1,26 @@
 (ns relevance.core
-  (:require [opennlp.nlp]
-            [opennlp.tools.filters]))
+  (:use [opennlp.nlp]
+        [opennlp.tools.filters]
+        [opennlp.treebank]))
 
+;; Tokenizers. Models are loaded from disk
 
+(def get-sentences 
+  (make-sentence-detector "models/en-sent.bin"))
+
+(def tokenize 
+  (make-tokenizer "models/en-token.bin"))
+
+(def pos-tag 
+  (make-pos-tagger "models/en-pos-maxent.bin"))
+
+(defn contains-term?
+  "Given a sentence, check if a term exist in that sentence"
+  [sentence term]
+  (let [tokens (tokenize sentence)]
+    (boolean (some #{term} tokens))))
+
+(defn extract-sentance-with-term [sentences term]
+  (->> sentences
+       (filter (fn [sent] (contains-term? sent term)))))
+  
