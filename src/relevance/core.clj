a5636a212127808b6f6d7fe4a65bcf04c4948bfc
owain lewis <owain@owainlewis.com>
2013-09-09 19:24:20 +0100

    doh running out of time

--- src/relevance/core.clj
+++ src/relevance/core.clj
@@ -5,6 +5,43 @@
 
 ;; Tokenizers. Models are loaded from disk
 
+;; CC Coordinating conjunction
+;; CD Cardinal number
+;; DT Determiner
+;; EX Existential there
+;; FW Foreign word
+;; IN Preposition or subordinating conjunction
+;; JJ Adjective
+;; JJR Adjective, comparative
+;; JJS Adjective, superlative
+;; LS List item marker
+;; MD Modal
+;; NN Noun, singular or mass
+;; NNS Noun, plural
+;; NNP Proper noun, singular
+;; NNPS Proper noun, plural
+;; PDT Predeterminer
+;; POS Possessive ending
+;; PRP Personal pronoun
+;; PRP$ Possessive pronoun
+;; RB Adverb
+;; RBR Adverb, comparative
+;; RBS Adverb, superlative
+;; RP Particle
+;; SYM Symbol
+;; TO to
+;; UH Interjection
+;; VB Verb, base form
+;; VBD Verb, past tense
+;; VBG Verb, gerund or present participle
+;; VBN Verb, past participle
+;; VBP Verb, non­3rd person singular present
+;; VBZ Verb, 3rd person singular present
+;; WDT Wh­determiner
+;; WP Wh­pronoun
+;; WP$ Possessive wh­pronoun
+;; WRB Wh­adverb
+
 (def get-sentences 
   (make-sentence-detector "models/en-sent.bin"))
 
@@ -22,5 +59,24 @@
 
 (defn extract-sentance-with-term [sentences term]
   (->> sentences
-       (filter (fn [sent] (contains-term? sent term)))))
+       (filter (fn [sentence] (contains-term? sentence term)))))
   
+(defn pos-tag-sentences 
+  ""
+  [sentences]
+  (map 
+    (fn [sentence] 
+      (pos-tag (tokenize sentence))) 
+        sentences))
+
+(defn pos-tag-text [text]
+  (let [sentences (get-sentences text)]
+    (pos-tag-sentences sentences)))
+
+(def noun? (fn [[_ tag]] (= tag "NNS")))
+
+(defn extract-tag [sentences tag]
+  (let [tags (pos-tag-sentences sentences)]
+    tags))
+
+;;    (map #(filter (fn [_ t] (= t tag)) %) tags)))
